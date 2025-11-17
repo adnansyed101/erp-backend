bcc0347c689940561f916fee78dd7c447a9a5935
adnansyed101 <sirgeant10@gmail.com>
2025-11-18 02:24:35 +0600

    Add route to get employee by id.

--- src/routes/employee.route.ts
+++ src/routes/employee.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createEmployee,
   getAllEmployees,
+  getEmployeeById,
 } from "../controllers/employee.controller.ts";
 
 const employeeRouter = express.Router();
@@ -9,5 +10,7 @@ const employeeRouter = express.Router();
 employeeRouter.get("/all-employees", getAllEmployees);
 
 employeeRouter.post("/create-employee", createEmployee);
+
+employeeRouter.get("/all-employees/:id", getEmployeeById);
 
 export default employeeRouter;
