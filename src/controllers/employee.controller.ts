4d6085043b60385fa490df5d6288eef39e9ceb07
adnansyed101 <sirgeant10@gmail.com>
2025-11-17 23:52:51 +0600

    Create controller to create employee.

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import prismaClient from "../db/prisma.ts";
+import type { Employee } from "../lib/types/types.ts";
 
 export const getAllEmployees = async (req: Request, res: Response) => {
   try {
@@ -15,6 +16,44 @@ export const getAllEmployees = async (req: Request, res: Response) => {
       success: false,
       message: "Error in fetching all employees",
       data: [],
+    });
+  }
+};
+
+export const createEmployee = async (req: Request, res: Response) => {
+  try {
+    const createdEmployee: Employee = await prismaClient.employee.create({
+      data: {
+        ...req.body,
+        presentAddress: {
+          create: {
+            ...req.body.presentAddress,
+          },
+        },
+        permanentAddress: {
+          create: {
+            ...req.body.permanentAddress,
+          },
+        },
+        spouseInformation: {
+          create: {
+            ...req.body.spouseInformation,
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Created Employee.",
+      data: createdEmployee,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      success: false,
+      message: "Error in fetching all employees",
+      data: {},
     });
   }
 };
