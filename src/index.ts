aa030767ae704a24d178e02f40b63a266663c249
adnansyed101 <sirgeant10@gmail.com>
2025-11-17 16:35:50 +0600

    Use employee route.

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import employeeRouter from "./routes/employee.route.ts";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Express with TypeScript!");
+app.use(express.json());
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello world");
 });
+
+app.use("/hr-management", employeeRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
